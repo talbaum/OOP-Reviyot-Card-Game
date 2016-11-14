5ea485611841f59c442f642a3d37e66ed6e4706e
Dunky <amitur91@gmail.com>
2016-11-14 23:03:47 +0200

    Add files via upload

--- include/Card.h
+++ include/Card.h
@@ -5,23 +5,25 @@
 using namespace std;
 
 enum Shape {
-	Club,
-	Diamond,
-	Heart,
-	Spade
+	Club='C',
+	Diamond='D',
+	Heart='H',
+	Spade='S'
 };
 
 enum Figure {
-	Jack,
-	Queen,
-	King,
-	Ace
+	Jack='J',
+	Queen='Q',
+	King='K',
+	Ace='A'
 };
 
 class Card {
 private:
   Shape shape;
 public:
+  Card();
+  Card(Shape s);
   virtual string toString() = 0; //Returns the string representation of the card "<value><shape>" exp: "12S" or "QD"
   virtual ~Card();
 };
@@ -31,12 +33,16 @@ private:
 	Figure figure;
 public:
 	virtual string toString() override;
+	FigureCard();
+	FigureCard(Shape,Figure);
 };
 
 class NumericCard : public Card {
 private:
 	int number;
 public:
+	NumericCard();
+	NumericCard(Shape,int);
 	virtual string toString() override;
 };
 
