a5c45d8a0911052860a172bd08bc967bdfcc3cdf
talbaum <talbaum16@gmail.com>
2016-11-15 21:13:42 +0200

    Add files via upload

--- revia/Hand.cpp
+++ revia/Hand.cpp
@@ -1,13 +1,118 @@
 #include "Hand.h"
+#include  <vector>
+#include <iostream>
+using namespace std;
 
+Hand:: Hand(); vector<vector<Card*>> HandCards() {   //empty constructor
+    int count=0;
+};
 
-Class Hand{
-	
-	Hand:: Hand() Vector<Vector<Card*>> HandCards : counter(0){
-		
-	}
-	
-	Hand:: Hand() Vector<Vector<Card*>> HandCards : counter(0){
-		
+Hand:: Hand(vector<Card*> temp); vector<vector<Card*>> HandCards() {
+    int count=temp.size();  //constructor that receives a vector<Cards*> from the Deck and make it a vector of vectors.
+    for (int i=0;i<temp.size();i++){
+            int pos = searchCard(temp[i]);
+                    HandCards[pos].push_back(temp[i]);
+            // if we already have a card from this value it will add to the back , if not it will be the first element.
+        }
+    for (int i=0;i<HandCards.size();i++){
+        if(HandCards[i].size()>0)
+            HandCards[i].sort();
+        // sort the inside vector by abc order (C-->D-->H-->S) which is the requested order for "strength" of shapes.
+    }
+
+};
+
+~Hand(){
+    delete (pos);//delete count?
+};
+
+int whereToInsert(Card &card){   // made for the addCard method.
+    int mid, left = 0 ;
+    int right = HandCards.size();
+    while (left < right) {
+        mid = left + (right - left)/2;
+        if (card.getValue() > HandCards[mid]){
+            left = mid+1;
+        }
+        else if (card.getValue() < HandCards[mid]){
+            right = mid;
+        }
+        else {
+            return mid;
+        }
+    }
+
+       return mid-1; //for making sure its inserted before the closer bigger value. need to check it.
+
+}
+~whereToInsert{
+        delete (right,mid,left)
+};
+
+    bool addCard(Card &card){
+        int pos=whereToInsert(card);
+        HandCards[pos].push_back(card);
+        HandCards[pos].sort();
+        count++;
+        return true;
+    }
+
+~addCard{
+delete(pos)
+};
+
+    bool removeCard(Card &card) {
+        int pos = searchCard(card);
+        if (pos < 0)
+            return false; //no such card at the hand.
+        else {
+            for (int i = 0; i < HandCards[pos].size(); ++i) {
+                if ((HandCards[pos])[i].getShape().equals(card.getShape())) {
+                    HandCards[pos].erase(i);
+                    count--;
+                    HandCards[pos].sort();
+                    return true;
+                }
+            }
+            return false;//the shape of this card is not in the inner vector.
+        }
+    }
+~removeCard{
+        delete(pos)
+};
+
+int searchCard(Card &card){  //made for remove card method. (had to make 2 diffrenet ones).
+    int mid, left = 0 ;
+    int right = HandCards.size();
+    while (left < right) {
+        mid = left + (right - left)/2;
+        if (card.getValue() > HandCards[mid]){
+            left = mid+1;
+        }
+        else if (card.getValue() < HandCards[mid]){
+            right = mid;
+        }
+        else {
+            return mid;
+        }
+    }
+    return -1; // if the exact value is not found at the vec.
+}
+
+~searchCard{
+        delete (right,mid,left)
+};
+
+	int getNumberOfCards(){
+	    return count;
+}
+~getNumberOfCards{}; //delete count?
+
+    string toString(){
+	for(int i=0; i<HandCards.size();i++){
+		cout << HandCards[i].toString()+ " " ;
 	}
 }
+~toString{};
+
+
--- revia/Hand.h
+++ revia/Hand.h
@@ -2,20 +2,24 @@
 #define HAND_H_
 
 #include <iostream>
-#include <vector>
+
 #include "Card.h"
 
 using namespace std;
 
 class Hand {
 private:
-	vector<vector<Card*> > HandCards;// Declare here the collection of "Card *" of the hand
-	int Counter;
+	vector<vector<Card*>> HandCards;// Declare here the collection of "Card *" of the hand
+	int count;
+	int searchCard(Card &card);
+	int whereToInsert(Card &card);
 public:
 	bool addCard(Card &card);
 	bool removeCard(Card &card);
+
 	int getNumberOfCards(); // Get the number of cards in hand
-	string toString(); // Return a list of the cards, separated by space, in one line, in a sorted order, ex: "2S 5D 10H"
+	string toString(); // Return a list of the cards, separated by space, in one line,
+	//in a sorted order, ex: "2S 5D 10H"
 };
 
 #endif
